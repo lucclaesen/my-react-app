422343d1968451eaf6400dab8d88e0cb4aa52028
Claesen <Luc.Claesen@dentsplysirona.com>
2019-07-03 11:48:54 +0200

    A much simpler solution, relying on lifecycles.

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,17 @@ import React from 'react';
 import './App.css';
 import ResizablePane from "./ResizablePane"
 
-type State = {
-  propagateMouseMoves: boolean;
-  clientX: number;
-}
-
-export default class App extends React.Component<{}, State> {
-
-  readonly state = { 
-    propagateMouseMoves: false,
-    clientX: 500
-  };
+export default class App extends React.Component{
 
   render() {
     return (
-      <div id="app" 
-        onMouseMove={this.onMouseMove} 
-        onMouseUp={() => this.propagateMouseMoves(false)}>
-        
-        <ResizablePane 
-            propagateMouseMoves={this.propagateMouseMoves}
-            clientX = {this.state.clientX}
-            />
-      
+      <div id="app">
+        <ResizablePane>
+          <div>
+            Pane content comes here ...
+          </div>
+        </ResizablePane>
       </div>
     );  
-  }
-
-  onMouseMove = (ev: React.MouseEvent) => {
-    if (this.state.propagateMouseMoves) {
-      const clientX = ev.clientX;
-      this.setState((currentState) => ({
-        ...currentState,
-        clientX
-      }));
-    }
-  };
-
-  propagateMouseMoves = (ev: boolean) => {
-    console.log(`About to ${ev? "start" : "stop"} following the mouse position.`);
-    this.setState(() => ({propagateMouseMoves: ev}));
   }
 }
--- src/ResizablePane.tsx
+++ src/ResizablePane.tsx
@@ -1,27 +1,59 @@
 import React from "react";
 
+type State = {
+  captureMouseMoves: boolean,
+  clientX?: number;
+};
 
-type Props =  {
-  clientX: number;
-  propagateMouseMoves: (ev: boolean) => void
-}
+type Props = {
+  children: React.ReactNode
+};
 
-export default class ResizablePane extends React.Component<Props> {
+export default class ResizablePane extends React.Component<Props, State> {
+
+  readonly state: State = {
+    captureMouseMoves: false,
+  }
 
   render() {
     return (
       <div className="resizable-pane" 
-        style={{width: this.props.clientX}}
->
+        style={{width: this.state.clientX}}>
         <div className="resize-handle" 
           onMouseDown={this.onMouseDown}>
         </div>
+        {this.props.children}
       </div>
     );
   }
 
+  componentDidUpdate(_: any, previousState: State) {
+    if (previousState.captureMouseMoves === this.state.captureMouseMoves) {
+      return;
+    }
+
+    if (this.state.captureMouseMoves) {
+      window.addEventListener("mouseup", this.onMouseUp);
+      window.addEventListener("mousemove", this.onMouseMove);
+    }
+    else {
+      window.removeEventListener("mousemove", this.onMouseMove);
+      window.removeEventListener("mouseup", this.onMouseUp);
+    }
+  }
+
   onMouseDown = () => {
     console.log("MouseDown event received");
-    this.props.propagateMouseMoves(true);
+    this.setState(() => ({captureMouseMoves: true}));
   } 
+
+  onMouseUp = () => {
+    console.log("MouseUp received on window");
+    this.setState(() => ({captureMouseMoves: false}));
+  }
+
+  onMouseMove = (ev: MouseEvent) => {
+    console.log("MouseMove event received");
+    this.setState(() => ({clientX: ev.clientX}));
+  }
 }
